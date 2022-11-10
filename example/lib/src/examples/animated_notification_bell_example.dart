4b192672fa22c5fab6299b6f214c740974de9c4e
Joey Boerwinkel <joey@iconica.nl>
2022-11-10 16:35:34 +0100

    feat: add animated notification icon

---
+++ example/lib/src/examples/animated_notification_bell_example.dart
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: 2022 Iconica
+//
+// SPDX-License-Identifier: BSD-3-Clause
+import 'package:flutter/material.dart';
+import 'package:flutter_animated_widgets/flutter_animated_widgets.dart';
+
+class AnimatedNoticationBellExample extends StatefulWidget {
+  const AnimatedNoticationBellExample({super.key});
+
+  @override
+  State<AnimatedNoticationBellExample> createState() =>
+      _AnimatedNoticationBellExampleState();
+}
+
+class _AnimatedNoticationBellExampleState
+    extends State<AnimatedNoticationBellExample> {
+  int notificationCount = 1;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 250,
+      child: Row(
+        children: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                notificationCount++;
+              });
+            },
+            icon: const Icon(Icons.add),
+          ),
+          AnimatedNotificationBell(
+            notificationCount: notificationCount,
+            notificationIconSize: 50,
+            iterations: notificationCount,
+            duration: const Duration(
+              milliseconds: 1000,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                notificationCount--;
+              });
+            },
+            icon: const Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
